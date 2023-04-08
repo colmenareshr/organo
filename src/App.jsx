ece5bcd9d3cae76c4644f7defed5018e81d5f55b
Humberto Colmenares <colmenareshr@gmail.com>
2023-04-08 13:02:56 -0300

    esconder times vazios

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
     <div className='App'>
       <Banner />
       <Formulario
-        times={times.map(time => time.nome)}
+        times={times.map((time) => time.nome)}
         colaborador={(colaborador) => colaboradorAdicionado(colaborador)}
       />
       {times.map((time) => (
@@ -60,7 +60,9 @@ function App() {
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
-          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time === time.nome
+          )}
         />
       ))}
     </div>
--- src/components/Time/indes.jsx
+++ src/components/Time/indes.jsx
@@ -3,19 +3,21 @@ import './Time.css'
 
 const Time = ({ nome, corPrimaria, corSecundaria, colaboradores }) => {
   return (
-    <section className='time' style={{ backgroundColor: corSecundaria }}>
-      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
-      <div className='colaboradores'>
-        {colaboradores.map((colaborador) => (
-          <Colaborador
-            key={colaborador.nome}
-            nome={colaborador.nome}
-            cargo={colaborador.cargo}
-            imagem={colaborador.imagem}
-          />
-        ))}
-      </div>
-    </section>
+    colaboradores.length > 0 && (
+      <section className='time' style={{ backgroundColor: corSecundaria }}>
+        <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+        <div className='colaboradores'>
+          {colaboradores.map((colaborador) => (
+            <Colaborador
+              key={colaborador.nome}
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    )
   )
 }
 
