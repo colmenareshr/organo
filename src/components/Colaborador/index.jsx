30e57d5e35e652979d1a36388dc4db68d1ccd762
Humberto Colmenares <colmenareshr@gmail.com>
2023-04-08 12:15:45 -0300

    Card Colaborador

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="colaborador-header">
+        <img src="https://github.com/colmenareshr.png" alt="Humberto Colmenares" />
+      </div>
+      <div className="colaborador-content">
+        <h4>Humberto Colmenares</h4>
+        <h5>Instructor</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
--- src/components/Time/indes.jsx
+++ src/components/Time/indes.jsx
@@ -1,9 +1,11 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = ({ nome, corPrimaria, corSecundaria }) => {
   return (
     <section className='time' style={{ backgroundColor: corSecundaria }}>
       <h3 style={{borderColor: corPrimaria}}>{nome}</h3>
+      <Colaborador />
     </section>
   )
 }
